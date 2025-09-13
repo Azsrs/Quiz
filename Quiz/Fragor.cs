89fa3424691ff6727729e9b20d84ce3c63779f5e
Azsrs <farzanehfrank@icloud.com>
2025-09-13 22:42:38 +0200

    kommentarer

--- Quiz/Fragor.cs
+++ Quiz/Fragor.cs
@@ -2,22 +2,22 @@
 public class Question //Klass för frågor
 {   public string QuestionText;
     public string Answer;
-    public bool Ask()
+    public bool Ask() //method som lämnar en bool för att kunna räkna poäng
     {
         Console.WriteLine(QuestionText);
         string PAnswer = Console.ReadLine().ToLower();
 
-        while (PAnswer != "a" && PAnswer != "b" && PAnswer != "c")
+        while (PAnswer != "a" && PAnswer != "b" && PAnswer != "c") //om man inte svarar
         {
             Console.WriteLine("Vänligen svara A, B eller C");
             PAnswer = Console.ReadLine().ToLower();
         }
-        if (PAnswer == Answer)
+        if (PAnswer == Answer) //om rätt
         {
             Console.WriteLine("Rätt! Snyggt gjort!");
             return true;
         }
-        else
+        else //om fel
         {
             Console.WriteLine("Fel svar!! Du är sämst!");
             return false;
--- Quiz/Program.cs
+++ Quiz/Program.cs
@@ -1,9 +1,9 @@
 ﻿
 int Points = 0;
 
-List<Question> questions = new List<Question>
+List<Question> questions = new List<Question> // lista där varje objekt är en instans av klass
 {
-new Question {
+new Question { //frågor byggda efter klassen Question
 QuestionText = "Hur många invånare bor i Iran \nA.92 miljoner B.27 miljoner C.150 miljoner" ,
 Answer = "a"
 },
@@ -16,25 +16,25 @@ QuestionText = "Vem är TE24Cs flitigaste, roligaste, snyggaste elev? \nA.Inte F
 Answer = "b"
 },
 };
-foreach (Question q in questions)
+foreach (Question q in questions) //loop ställer frågor från listan till listana är slut
 {
     if (q.Ask())
     {
         Points++;
     }
-    Console.WriteLine($"Du har nu {Points} poäng. \nTryck ENTER för att fortsätta");
+    Console.WriteLine($"Du har nu {Points} poäng. \nTryck ENTER för att fortsätta"); //uppmanar om poängantal, förklarar hur man går vidare
     Console.ReadLine();
 }
-Console.WriteLine("Det var det! \nVi har räknat ihop dina poäng och kommit fram till en slutsats");
+Console.WriteLine("Det var det! \nVi har räknat ihop dina poäng och kommit fram till en slutsats"); //fake loading screen för komisk effekt
 Console.ReadLine();
 Console.WriteLine("loading..");
 Thread.Sleep(300);
-if (Points <= 2)
+if (Points <= 2) //otroligt få poäng
 {
     Console.WriteLine("Du är sämst!");
     Console.ReadLine();
 }
-else if (Points == questions.Count)
+else if (Points == questions.Count) //alla rätt
 {
     Console.WriteLine("Snyggt gjort! Alla rätt!!!");
     Console.ReadLine();
